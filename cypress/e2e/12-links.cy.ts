671c6728b5d1ad63d9f48032f1edfdef2af08821
TECGRAF\danielabrazao <daniela.brazao.maksoud@gmail.com>
2025-01-21 14:28:38 -0300

    Terminando seção 9.

--- cypress/e2e/12-links.cy.ts
+++ cypress/e2e/12-links.cy.ts
@@ -6,10 +6,26 @@ describe('Dealing with links that open new tabs', () => {
         cy.get('#simpleLink').should('have.attr', 'href', 'https://demoqa.com');
         cy.get('#simpleLink').should('have.attr', 'target', '_blank');
     });
-    it('Second approach, remove the target', () => {
+    it.skip('Second approach, remove the target', () => {
         cy.get('#simpleLink').invoke('removeAttr', 'target').click();
         cy.url().then((url) => {
             expect(url).to.be.equal('https://demoqa.com/');
+        });
+    });
+
+});
+
+describe.only('Intercepting API (SPYING) requests after clicking on a button', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("demoQA")}/links`);
+        cy.intercept('GET', `${Cypress.env("demoQA")}/created`).as('linkStatus');
+    });
+    it('First approach, don´t click on the link', () => {
+        cy.get('a#created').click();
+        cy.wait('@linkStatus').then((request) => {
+            cy.log('This is the request intercepted', request);
+            expect(request.response.statusCode).to.be.equal(201);
+            expect(request.response.statusMessage).to.be.equal("Created");
         });
     });
 });
