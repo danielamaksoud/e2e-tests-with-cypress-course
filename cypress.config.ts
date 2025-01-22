0dd77b362a714655fd23444d0e8f1c437c43f467
TECGRAF\danielabrazao <daniela.brazao.maksoud@gmail.com>
2025-01-22 13:22:35 -0300

    Fazendo exercício 76.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "cypress";
+// Verify download import
+const { isFileExist, findFiles } = require('cy-verify-downloads');
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://uitestingplayground.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Verify download import
+      on('task', { isFileExist, findFiles })
+      // -----------------------
     },
     env: {
       demoVar: "Hello from the Cypress.Config.ts",
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,4 +1,5 @@
 require('@cypress/xpath');
+require("cy-verify-downloads").addCustomCommand();
 import "cypress-real-events/support";
 import "./exceptions";
 import "cypress-file-upload";
